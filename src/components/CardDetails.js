3731b3b30af000a64927f5af78c8a62c57dec734
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-10-28 23:23:46 +0100

    ch(init)
    - initialize project

---
+++ src/components/CardDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CardDetails = () => {
+  return (
+    <div>
+      <div>Card Number</div>
+      <div>Card Holder</div>
+      <div>Expiration Date</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CardDetails;
